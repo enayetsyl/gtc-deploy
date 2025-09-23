852978f385b84a352c7100500002843f9e8777ed
enayetsyl <enayetflweb@gmail.com>
2025-09-23 18:57:48 +0600

    backend deploy file done

--- apps/api/src/lib/mailer.ts
+++ apps/api/src/lib/mailer.ts
@@ -3,5 +3,9 @@ import nodemailer from "nodemailer";
 export const mailer = nodemailer.createTransport({
   host: process.env.MAIL_HOST || "127.0.0.1",
   port: Number(process.env.MAIL_PORT || 1025),
-  secure: false,
+  secure: process.env.MAIL_PORT === "465", // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
 });
