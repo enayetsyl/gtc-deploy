36d0411a3177f93dcdacfc48c870a8a93921872c
enayetsyl <enayetflweb@gmail.com>
2025-10-03 16:12:46 +0600

    Chore: Prod migration fix

--- apps/api/prisma/migrations/20251003094221_prod_schema_changes/migration.sql
+++ apps/api/prisma/migrations/20251003094221_prod_schema_changes/migration.sql
@@ -1,2 +1,4 @@
 -- RenameIndex
-ALTER TABLE `GtcPointSector` RENAME INDEX `GtcPointSector_gtcPointId_sectorId_unique` TO `GtcPointSector_gtcPointId_sectorId_key`;
+-- MariaDB doesn't support RENAME INDEX in all versions. Replace by dropping the old unique index and recreating it with the desired name.
+ALTER TABLE `GtcPointSector` DROP INDEX `GtcPointSector_gtcPointId_sectorId_unique`;
+ALTER TABLE `GtcPointSector` ADD UNIQUE INDEX `GtcPointSector_gtcPointId_sectorId_key` (`gtcPointId`, `sectorId`);
